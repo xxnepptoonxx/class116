87c86231247441d17ec2ada2fe161624a62d6e0d
xxnepptoonxx <113563351+xxnepptoonxx@users.noreply.github.com>
2023-05-08 16:26:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/VvpDTCm9/d3086558665c07c38cc8ebe8ed33003a.jpg')
+}
+
+function setup() {
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+ {
+    console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+ }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+   image(clown_nose, noseX, noseY, 30, 30);
+}
